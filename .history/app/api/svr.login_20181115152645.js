404affbefce53c4768df9ead198ace143dd4e581
mamtabhardwaj12 <aniket@cateina.com>
2018-11-21 14:07:22 +0530

    changes in srv.login.js

---
+++ .history/app/api/svr.login_20181115152645.js
@@ -0,0 +1,195 @@
+/**
+ * Created By :- Akshay
+ * Created Date :- 29-11-2017 01:00 pm
+ * Version :- 1.0.0
+ */
+var config = require('../../config/config.json');                       // call configration file
+var _ = require('lodash');                                              // Load the full build. for manipulating objects and collections
+var jwt = require('jsonwebtoken');                                      // for creating token
+var bcrypt = require('bcryptjs');                                       // for hashing
+var Q = require('q');                                                   // for promise 
+var TinyURL = require('tinyurl')
+var mongo = require('mongoskin');                                       // call mongodb    
+var db = mongo.db(config.connectionString, { native_parser: true });    // mongodb connectivity
+// db.bind('SCF');                                                       // bind the collection
+
+var service = {};
+
+service.authenticate = authenticate;
+service.getUser = getUser;
+service.changePW = changePW;
+service.forgotPW = forgotPW;
+service.forgotUpdatePW = forgotUpdatePW;
+
+module.exports = service;
+
+function authenticate(req, res) {
+    console.log("inside auth service==>>>",req.query);
+    var deferred = Q.defer();
+
+    if (!req.body.username) { deferred.reject({ error: "please enter username" }) }
+    if (!req.body.password) { deferred.reject({ error: "please enter password" }) }
+    if (!req.body.appName) { deferred.reject({ error: "please enter application name" }) }
+
+    var username = req.body.username;
+    var password = req.body.password;
+    // console.log("password", password);
+    var collectionName = req.body.appName;                              // create the collection based on appName
+    db.bind(collectionName);                                            // bind the collection based on appName
+    
+    db.collection(collectionName).findOne({ username: username }, function (err, user) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+        console.log("user obj<><>", user);
+        if (user && bcrypt.compareSync(password, user.hash)) {
+            deferred.resolve(jwt.sign({ sub: user._id}, config.secret), user);    // authentication successful
+        } else {
+            deferred.resolve();                                              // authentication failed
+        }
+    });
+    return deferred.promise;
+}
+
+
+function getUser(req, res) {
+    var deferred = Q.defer();
+    if (!req.query.username) { deferred.reject({ error: "please enter username" }) }
+    if (!req.query.appName) { deferred.reject({ error: "please enter application name" }) }
+
+    var username = req.query.username;
+    var collectionName = req.query.appName;                              // create the collection based on appName
+    db.bind(collectionName);                                            // bind the collection based on appName
+    db.collection(collectionName).findOne({ username: username }, function (err, user) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        deferred.resolve(user);
+    });
+    return deferred.promise;
+}
+
+/**
+ * @author Akshay Misal
+ * @param { } req
+ * @param { } res
+ * @description change password
+ */
+function changePW(req, res) {
+    var deferred = Q.defer();
+
+    if (!req.body.username) { deferred.reject({ error: "please enter username" }) }
+    if (!req.body.appName) { deferred.reject({ error: "please enter application name" }) }
+    if (!req.body.oldPassword) { deferred.reject({ error: "please enter oldPassword" }) }
+    if (!req.body.newPassword) { deferred.reject({ error: "please enter newPassword" }) }
+    if (!req.body.confirmPassword) { deferred.reject({ error: "please enter confirmPassword" }) }
+
+    if (req.body.confirmPassword !== req.body.newPassword) {
+        deferred.reject({ error: "oldPassword & confirmPassword does not match." })
+    } else {
+        var username = req.body.username;
+        var collectionName = req.body.appName;                              // create the collection based on appName
+        var oldPassword = req.body.oldPassword;
+
+        db.bind(collectionName);                                            // bind the collection based on appName
+
+        db.collection(collectionName).findOne({ username: username }, function (err, user) {
+
+            if (err) deferred.reject(err.name + ': ' + err.message);
+
+            if (user && bcrypt.compareSync(oldPassword, user.hash)) {
+                deferred.resolve(updatePW(req, res));
+            } else {
+                deferred.reject({ error: "old password is wrong." })
+            }
+
+        });
+    }
+    return deferred.promise;
+}
+
+
+function updatePW(req, res) {
+    var deferred = Q.defer();
+
+    var username = req.body.username;
+    var collectionName = req.body.appName;                              // create the collection based on appName
+    var confirmPassword = req.body.confirmPassword;
+
+    var hash = bcrypt.hashSync(confirmPassword, 10);             // add hashed password to user object with salt
+    var set = {
+        hash: hash
+    };
+
+    db.bind(collectionName);                                            // bind the collection based on appName
+
+    db.collection(collectionName).update({ username: username }, { $set: set },
+        function (err, doc) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+            deferred.resolve({ message: "Successfully change the password." });
+        })
+    return deferred.promise;
+}
+
+/**
+ * @author Akshay Misal
+ * @description This function will first check email. This function will create the Tiny-Url.
+ */
+function forgotPW(req, res) {
+    var deferred = Q.defer();
+
+    if (!req.body.email) { deferred.reject({ error: "please enter email" }) }
+    if (!req.body.appName) { deferred.reject({ error: "please enter application name" }) }
+    if (!req.body.url) { deferred.reject({ error: "please enter application URL" }) }
+
+    var email = req.body.email;
+    var collectionName = req.body.appName;                              // create the collection based on appName    
+
+    db.bind(collectionName);
+
+    db.collection(collectionName).findOne({ email: email }, function (err, user) {
+
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (user) {
+            var url = req.body.url;
+            TinyURL.shorten(url, function (res, err) {
+                deferred.resolve({ message: "Success", url: res })
+            });
+        } else {
+            deferred.reject({ error: "email not found." })
+        }
+
+    });
+
+    return deferred.promise;
+}
+
+/**
+ * @author Akshay Misal
+ * @description This function will change the password based on tinyURL.
+ */
+function forgotUpdatePW(req, res) {
+    var deferred = Q.defer();
+
+    if (!req.body.appName) { deferred.reject({ error: "please enter application name" }) }
+    if (!req.body.username) { deferred.reject({ error: "please enter username" }) }
+    if (!req.body.confirmPassword) { deferred.reject({ error: "please enter application confirm password" }) }
+    if (!req.body.newPassword) { deferred.reject({ error: "please enter application new password" }) }
+
+    var username = req.body.username;
+    var confirmPassword = req.body.confirmPassword;
+    var collectionName = req.body.appName;
+
+    var hash = bcrypt.hashSync(confirmPassword, 10);             // add hashed password to user object with salt
+    var set = {
+        hash: hash
+    };
+
+    db.bind(collectionName);                                            // bind the collection based on appName
+
+    db.collection(collectionName).update({ username: username }, { $set: set },
+        function (err, doc) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+            deferred.resolve({ message: "Successfully change the password." });
+        })
+
+    return deferred.promise;
+}
---
+++ .history/server/server_20181103150005.js
@@ -0,0 +1,128 @@
+/**
+ * Created By :- Akshay
+ * Created Date :- 09-11-2017 05:00 am
+ * Version :- 1.0.0
+ * Updated By :- Akshay
+ * Updated Date :- 29-11-2017 10:00 pm
+ * Version :- 1.1.0 
+ */
+// call the packages we need
+var express = require('express');                       // call express
+var app = express();                                    // define our app using express
+var bodyParser = require('body-parser');                // configure app to use router()
+var router = express.Router();                          // get an instance of the express Router
+var userService = require('../app/api/svr.login');       // call userservice 
+var config = require('./../config/config.json');           // call configration file
+var port = process.env.PORT || config.port;             // set our port
+var swaggerUi = require('swagger-ui-express');
+var swaggerDocument = require('./../doc/swagger.json'); //Path of swagger.json file in your app directory
+var morgan = require('morgan');
+var winston = require('./../config/wintston');
+
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Expose your swagger documentation through your express framework
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); 
+
+
+app.use(morgan('combined', { stream: winston.stream }));
+
+// error handler for logging using winston
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // add this line to include winston logging
+    winston.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+  
+    // render the error page
+    res.status(err.status || 500);
+    // next();
+    res.send('error');
+  });
+
+
+
+router.post('/login', authenticateUsers);          //authentication is done here (http://localhost:8082/api/)
+router.get('/login', getUser);                   //authentication is done here (http://localhost:8082/api/)
+router.put('/login', changePW);                   //authentication is done here (http://localhost:8082/api/)
+router.post('/login/forgotPassword',forgotPW);  //authentication is done here (http://localhost:8082/login)
+router.put('/login/forgotPassword',forgotUpdatePW);   //authentication is done here (http://localhost:8082/login)
+
+module.exports = router;
+
+function authenticateUser(req, res) {
+    console.log("auth",req.query)
+    userService.authenticate(req,res)
+        .then(function (token) {
+            if (token) {
+                res.send({ 
+                    message : 'Login Successful.',         // authentication successful and send token
+                    token : token
+                });
+            } else {                                       // authentication failed
+                res.status(401).send({message : 'Username or password is incorrect'});
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function getUser(req, res) {
+    console.log("get ",req.query)
+    userService.getUser(req,res)
+        .then(function (data) {                                 
+            res.send(data);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function changePW(req, res) {
+    userService.changePW(req,res)
+        .then(function (data) {                                 
+            res.send(data);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function forgotPW(req, res) {
+    userService.forgotPW(req,res)
+        .then(function (data) {                                 
+            res.send(data);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function forgotUpdatePW(req, res) {
+    userService.forgotUpdatePW(req,res)
+        .then(function (data) {                                 
+            res.send(data);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+router.get('/', function (req, res) {                       //for testing the api service (http://localhost:8082/)
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+//---------------------------REGISTER OUR ROUTES ---------------------------
+app.use('', router);                                    // all of our routes will be prefixed with /api
+
+// ================= START THE SERVER=======================================
+var server = app.listen(port, function () {
+    console.log('Server listening at http://localhost:' + server.address().port);
+});
+
+module.exports = server
